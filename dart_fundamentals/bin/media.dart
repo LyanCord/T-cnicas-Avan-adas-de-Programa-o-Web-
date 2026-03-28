b2e8846bf3c1134048112668c4173562ae9ccc03
Lyan Quiliom Divanir Cordeiro <158008971+LyanCord@users.noreply.github.com>
2026-03-28 22:18:00 +0000

    Atividade 5 exercicios

---
+++ dart_fundamentals/bin/media.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(){
+  stdout.write('Qual o valor da nota da prova?');
+  String? valor1 = stdin.readLineSync();
+  double? prova = double.tryparce(valor1!) ?? 0.0;
+
+  stdout.write('Qual a nota do trabalho?');
+  double? trabalho = double.tryparce(stdin.readLineSync()!)?? 0.0;
+
+  double? media = prova * 0.6 + trabalho * 0.4;
+
+  stdout.write('A sua média é $media\n');
+}
+
